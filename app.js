a1eccf682c3bd6410d01c912938fe9c9dc3b58a3
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2020-12-28 16:13:15 +0900

    feat: 박물관 벽 만들기 - 자세하게 보여주기

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
+const wall = document.getElementById("jsWall");
+const picture1 = document.getElementById("jsPicture1");
+const picture2 = document.getElementById("jsPicture2");
+const picture3 = document.getElementById("jsPicture3");
+const description1 = document.getElementById("jsDescription1");
+const description2 = document.getElementById("jsDescription2");
+const description3 = document.getElementById("jsDescription3");
+const singlePicture = document.getElementById("jsSinglePicture");
+const singlePictureBackBtn = document.getElementById("jsSinglePictureBackBtn");
+
 class App {
     constructor() {
-        this.picture1 = document.getElementById("jsPicture1");
-        this.picture2 = document.getElementById("jsPicture2");
-        this.picture3 = document.getElementById("jsPicture3");
-        this.description1 = document.getElementById("jsDescription1");
-        this.description2 = document.getElementById("jsDescription2");
-        this.description3 = document.getElementById("jsDescription3");
-
         this.writeDescription();
+        picture1.addEventListener("click", this.clickPicture1);
+        picture2.addEventListener("click", this.clickPicture2);
+        picture3.addEventListener("click", this.clickPicture3);
+        singlePictureBackBtn.addEventListener("click", this.clickBack);
     }
 
     writeDescription() {
-        this.description1.innerHTML = "<br> Müller-Lyer Illusion <br> <strong> 1889 </strong>";
-        this.description2.innerHTML = "<br> Ponzo Illusion <br> <strong> 1911 </strong>";
-        this.description3.innerHTML = "<br> Ehrenstein Illusion <br> <strong> 1841 </strong>";
+        description1.innerHTML = "<br> Müller-Lyer Illusion <br> <strong> 1889 </strong>";
+        description2.innerHTML = "<br> Ponzo Illusion <br> <strong> 1911 </strong>";
+        description3.innerHTML = "<br> Ehrenstein Illusion <br> <strong> 1841 </strong>";
+    }
+
+    clickPicture1() {
+        wall.style.display = "none";
+        singlePicture.style.display = "flex";
+    }
+
+    clickPicture2() {
+        wall.style.display = "none";
+        singlePicture.style.display = "flex";
+    }
+
+    clickPicture3() {
+        wall.style.display = "none";
+        singlePicture.style.display = "flex";
+    }
+
+    clickBack() {
+        wall.style.display = "flex";
+        singlePicture.style.display = "none";
     }
 }
 
